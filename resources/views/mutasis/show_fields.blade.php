9cf467f16ec4915870e70df3c0dc8c5a0b61fbad
toiras87 <toirazone@yahoo.com>
2019-12-09 21:49:15 +0700

    penambah view documen di konfirmasi mutasi (step terakhir) -> failed dapat filenya

--- resources/views/mutasis/show_fields.blade.php
+++ resources/views/mutasis/show_fields.blade.php
@@ -65,32 +65,6 @@
     </table>
 </div>
 
-<button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4> 
-      </div>
-      <div class="modal-body">
-        <div style="text-align: center;">
-<iframe src="http://docs.google.com/gview?url=http://www.pdf995.com/samples/pdf.pdf&embedded=true" 
-style="width:500px; height:500px;" frameborder="0"></iframe>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
 <script> 
 
 
@@ -153,15 +127,15 @@ if ( ! $.fn.DataTable.isDataTable( '#table-dokumen-mutasi-<?= $uniqID  ?>' ) ) {
                 title: 'Nama Dokumen',
                 orderable: false,
             },
-            {
+            /*{
                 data: 'dokumenPath',
                 title: 'Lokasi Dokumen',
                 orderable: false,
-            },
+            },*/
             {
             data: null,
             render: function ( data, type, row ) {
-            return '<!-- Button trigger modal --><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#viewDocModal">Lihat Dokumen</button><!-- Modal --><div class="modal fade" id="viewDocModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title" id="myModalLabel">'+data.dokumenNama+'</h4></div><div class="modal-body"><div style="text-align: center;"><iframe src="http://docs.google.com/gview?url={{ url(Storage::url('+data.dokumenPath+')) }}&embedded=true" style="width:500px; height:500px;" frameborder="0"></iframe></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+            return `<!-- Button trigger modal --><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#viewDocModal">Lihat Dokumen</button><!-- Modal --><div class="modal fade" id="viewDocModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title" id="myModalLabel">'${data.dokumenNama}'</h4></div><div class="modal-body"><div style="text-align: center;"><iframe src="http://docs.google.com/gview?url={{ url(Storage::url('${data.dokumenPath}')) }}&embedded=true" style="width:500px; height:500px;" frameborder="0"></iframe></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>`;
             }
             },
         ],
