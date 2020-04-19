be4dadfa9801dad3fe53bba5f9be489a257b07f7
Anggito Wibisono Putro <anggito@kanosolution.com>
2020-04-19 17:57:26 +0700

    fixed typo

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -14,7 +14,7 @@
 
             <div class="box-body">
                 <div class="">
-                    {!! Form::open(['route' => 'users.store', , 'autocomplete' => 'new-password']) !!}
+                    {!! Form::open(['route' => 'users.store', 'autocomplete' => 'new-password']) !!}
 
                         @include('users.fields')
 
