5e459a2b969b12e9765d2dd71bcb4864a1841f49
unknown <hendikedison09@gmail.com>
2021-02-15 17:55:12 +0700

    - Laporan Daftar Barang Ekstrakomp - Laporan Daftar Mutasi Tambah - Laporan Daftar Mutasi Kurang - Lampiran BAST Mutasi - Lampiran Surat Usulan

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,4 +15,34 @@ class ReportController extends AppBaseController
         
         return view('report.DaftarBarang');
     }
+
+    public function DaftarBarangIntraKomp(Request $request) {
+        
+        return view('report.DaftarBarangIntrakomp');
+    }
+
+    public function DaftarBarangEkstraKomp(Request $request) {
+        
+        return view('report.DaftarBarangEkstrakomp');
+    }
+
+    public function DaftarMutasiTambah(Request $request) {
+        
+        return view('report.DaftarMutasiTambah');
+    }
+    
+    public function DaftarMutasiKurang(Request $request) {
+        
+        return view('report.DaftarMutasiKurang');
+    }
+    
+    public function LampiranBASTMutasi(Request $request) {
+        
+        return view('report.LampiranBASTMutasi');
+    }
+    
+    public function LampiranSuratUsulan(Request $request) {
+        
+        return view('report.LampiranSuratUsulan');
+    } 
 }
